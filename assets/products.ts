9b9c36ed57d58724d449ddd3506755501d8f6f04
Michael Poirier-Ginter <michael.poirierginter@gmail.com>
2020-06-17 15:53:41 -0400

    Added new product

--- assets/products.ts
+++ assets/products.ts
@@ -25,6 +25,17 @@ const Products: Product[] = [
     fileGuid: "09e589c7-3d18-4c2e-9cee-4f447e9f054a"
   },
   {
+    id: "demo-rosy",
+    name: "Rosy-Fingered Dawn at Louse Point",
+    prices: {
+      physical: 49.95,
+      digital: 29.75
+    },
+    description: "The title Rosy-Fingered Dawn at Louse Point refers to one of Willem de Kooning's favourite places in Long Island, New York. During his period in Long Island De Kooning rode his bike daily to Louse Point where he observed the water.",
+    image: "/images/rosy.jpg",
+    fileGuid: "98dbd416-159d-4f76-8a59-30703cf6189f"
+  },
+  {
     id: "demo-irises",
     name: "Irises",
     prices: {
