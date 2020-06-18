ac306261e0c4f4460598336298c62666cbe9c5e4
Michael Poirier-Ginter <michael.poirierginter@gmail.com>
2020-06-18 15:22:21 -0400

    Added favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
       },
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "icon", type: "image/png", href: "/favicon.png" },
       {
         rel: "stylesheet",
         href:
